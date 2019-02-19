00a376b999468374684ccaf6b0e2739444864178
Summers Pittman <secondsun@gmail.com>
2019-02-19 08:18:09 -0500

    working

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 import gql from 'graphql-tag';
-import { init } from '@aerogear/app';
+const { init }  = require("@aerogear/app");
 
 import {
-  createClient
+  createClient,
 } from '@aerogear/voyager-client';
 
+const app = init({
+  "version": 1,
+  "namespace": "myproject",
+  "clientId": "getting-started",
+  "services": [
+    {
+      "id": "0637bfd3-33aa-11e9-968e-52540014a8c2",
+      "name": "sync-app-getting-started-getting-started",
+      "type": "sync-app",
+      "url": "https://sync-app-getting-started-myproject.192.168.42.138.nip.io/graphql",
+      "config": {
+        "websocketUrl": "wss://sync-app-getting-started-myproject.192.168.42.138.nip.io/graphql"
+      }
+    }
+  ]
+});
 
 async function helloWorld() {
   
   let client = await createClient({
-    openShiftConfig:{
-      "version": 1,
-      "namespace": "myproject",
-      "clientId": "getting-started",
-      "services": [
-        {
-          "id": "72197937-3382-11e9-968e-52540014a8c2",
-          "name": "sync-app-getting-started-getting-started",
-          "type": "sync-app",
-          "url": "https://sync-app-getting-started-myproject.192.168.42.138.nip.io/graphql",
-          "config": {
-            "websocketUrl": "wss://sync-app-getting-started-myproject.192.168.42.138.nip.io/graphql"
-          }
-        }
-      ]
-    }
+    openShiftConfig:app.config
   });
   client.query({
       fetchPolicy: 'network-only',
